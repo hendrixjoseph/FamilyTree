3cd016f8b6a6eb034af5d1116935fc4828bda3b2
Joseph Hendrix <hendrixjoseph@aol.com>
2015-07-07 23:55:09 -0400

    Added trigger for inserting into PERSON_VIEW
    Made mainTemplate.xhtml

--- sql/master.sql
+++ sql/master.sql
@@ -1,16 +1,16 @@
 --------------------------------------------------------
---  File created - Monday-July-06-2015   
+--  File created - Tuesday-July-07-2015   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Sequence PLACE_SEQUENCE
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "PLACE_SEQUENCE"  MINVALUE 1 MAXVALUE 99999999 INCREMENT BY 1 START WITH 21 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "PLACE_SEQUENCE"  MINVALUE 1 MAXVALUE 99999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Sequence SEQUENCE_PERSON
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "SEQUENCE_PERSON"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 41 CACHE 20 NOORDER  NOCYCLE ;
+   CREATE SEQUENCE  "SEQUENCE_PERSON"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE ;
 --------------------------------------------------------
 --  DDL for Table BIRTH
 --------------------------------------------------------
@@ -214,6 +214,7 @@ Insert into PLACE (ID,NAME) values (3,'Jacksonville, Ohio');
 --  Constraints for Table FATHER_OF
 --------------------------------------------------------
 
+  ALTER TABLE "FATHER_OF" MODIFY ("FATHER_ID" NOT NULL ENABLE);
   ALTER TABLE "FATHER_OF" ADD CONSTRAINT "FATHER_OF_PK" PRIMARY KEY ("CHILD_ID") ENABLE;
   ALTER TABLE "FATHER_OF" MODIFY ("CHILD_ID" NOT NULL ENABLE);
 --------------------------------------------------------
@@ -311,6 +312,87 @@ END;
 /
 ALTER TRIGGER "PERSON_SEQ_TRIGGER" ENABLE;
 --------------------------------------------------------
+--  DDL for Trigger PERSON_VIEW_INSERT_TRIGGER
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "PERSON_VIEW_INSERT_TRIGGER" 
+INSTEAD OF INSERT ON PERSON_VIEW 
+DECLARE
+  GENDER_ABBR CHAR(1);
+  P_ID NUMBER;
+  PLACE_OF_BIRTH_ID NUMBER;
+  PLACE_OF_DEATH_ID NUMBER;
+BEGIN
+  -- Insert the person
+  SELECT ABBR INTO GENDER_ABBR FROM JOE.GENDER WHERE GENDER.FULL_WORD=:new.GENDER;
+  INSERT INTO PERSON (NAME, GENDER) VALUES (:new.NAME, GENDER_ABBR);
+  
+  SELECT MAX(ID) INTO P_ID FROM PERSON; 
+  
+  -- Map FATHER_ID to FATHER_OF table
+  IF :new.FATHER_ID IS NOT NULL THEN
+    INSERT INTO FATHER_OF (FATHER_ID, CHILD_ID) VALUES (:new.FATHER_ID, P_ID);
+  END IF;
+  
+  -- Map MOTHER_ID to MOTHER_OF table
+  IF :new.MOTHER_ID IS NOT NULL THEN
+    INSERT INTO MOTHER_OF (MOTHER_ID, CHILD_ID) VALUES (:new.MOTHER_ID, P_ID);
+  END IF;
+  
+  -- Insert Birth
+  
+  -- First, map place
+  IF :new.PLACE_OF_BIRTH IS NOT NULL THEN
+    BEGIN
+      SELECT ID INTO PLACE_OF_BIRTH_ID FROM PLACE WHERE :new.PLACE_OF_BIRTH=PLACE.NAME;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+      INSERT INTO PLACE (NAME) VALUES (:new.PLACE_OF_BIRTH);
+      SELECT ID INTO PLACE_OF_BIRTH_ID FROM PLACE WHERE :new.PLACE_OF_BIRTH=PLACE.NAME;
+    END;
+    INSERT INTO BIRTH (PERSON_ID, PLACE_ID) VALUES (P_ID, PLACE_OF_BIRTH_ID);
+  END IF;
+  
+  -- Now, insert date
+  IF :new.DATE_OF_BIRTH IS NOT NULL THEN
+    BEGIN
+      -- This select does nothing except throw the exception if there is
+      -- no birth record yet. Update won't throw it for some reason.
+      SELECT PERSON_ID INTO P_ID FROM BIRTH WHERE PERSON_ID=P_ID;
+      UPDATE BIRTH SET "DATE"=:new.DATE_OF_BIRTH WHERE PERSON_ID=P_ID;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+      INSERT INTO BIRTH (PERSON_ID, "DATE") VALUES (P_ID, :new.DATE_OF_BIRTH);
+    END;
+  END IF;
+  
+  -- Insert Death
+  
+  -- First, map place
+  IF :new.PLACE_OF_DEATH IS NOT NULL THEN
+    BEGIN
+      SELECT ID INTO PLACE_OF_DEATH_ID FROM PLACE WHERE :new.PLACE_OF_DEATH=PLACE.NAME;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+      INSERT INTO PLACE (NAME) VALUES (:new.PLACE_OF_DEATH);
+      SELECT ID INTO PLACE_OF_DEATH_ID FROM PLACE WHERE :new.PLACE_OF_DEATH=PLACE.NAME;
+    END;
+    INSERT INTO DEATH (PERSON_ID, PLACE_ID) VALUES (P_ID, PLACE_OF_DEATH_ID);
+  END IF;
+  
+  -- Now, insert date
+  IF :new.DATE_OF_DEATH IS NOT NULL THEN
+    BEGIN
+      -- This select does nothing except throw the exception if there is
+      -- no death record yet. Update won't throw it for some reason.
+      SELECT PERSON_ID INTO P_ID FROM DEATH WHERE PERSON_ID=P_ID;
+      UPDATE DEATH SET "DATE"=:new.DATE_OF_DEATH WHERE PERSON_ID=P_ID;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+      INSERT INTO DEATH (PERSON_ID, "DATE") VALUES (P_ID, :new.DATE_OF_DEATH);
+    END;
+  END IF;
+  
+END;
+/
+ALTER TRIGGER "PERSON_VIEW_INSERT_TRIGGER" ENABLE;
+--------------------------------------------------------
 --  DDL for Trigger PLACE_SEQ_TRIGGER
 --------------------------------------------------------
 
@@ -325,18 +407,3 @@ BEGIN
 END;
 /
 ALTER TRIGGER "PLACE_SEQ_TRIGGER" ENABLE;
---------------------------------------------------------
---  DDL for Procedure INSERT_PERSON
---------------------------------------------------------
-set define off;
-
-  CREATE OR REPLACE PROCEDURE "INSERT_PERSON" 
-(
-  NAME IN VARCHAR2 
-, FATHER_ID IN NUMBER 
-) AS 
-BEGIN
-  NULL;
-END INSERT_PERSON;
-
-/
