2309964f241319a87ecda226bb73e74e2faac464
Joe Hendrix <hendrixjoseph@aol.com>
2015-07-21 10:20:21 -0400

    Create Gender.java

---
+++ src/java/edu/wright/hendrix11/familyTree/entity/Gender.java
@@ -0,0 +1,31 @@
+package edu.wright.hendrix11.familyTree.entity;
+
+/**
+*
+* @author Joe Hendrix
+*/
+public class Gender 
+{
+    private char abbr;
+    private String fullWord;
+
+    public char getAbbr()
+    {
+        return abbr;
+    }
+
+    public void setAbbr(char abbr)
+    {
+        this.abbr = abbr;
+    }
+
+    public String getFullWord()
+    {
+        return fullWord;
+    }
+
+    public void setFullWord(String fullWord)
+    {
+        this.fullWord = fullWord;
+    }      
+}
