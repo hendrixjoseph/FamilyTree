17380a7eb56f3d032abb5179d5bdf8ed19cbb0ae
Joe Hendrix <hendrixjoseph@aol.com>
2015-07-21 16:06:29 -0400

    Create XmlImporter.java

---
+++ src/java/edu/wright/hendrix11/familyTree/database/import/XmlImporter.java
@@ -0,0 +1,14 @@
+
+package edu.wright.hendrix11.familyTree.database.import;
+
+/**
+*
+* @author Joe Hendrix
+*/
+public class XmlImporter extends Importer
+{
+    public void import()
+    {
+    
+    }
+}
