a30620d449a25a951f43ca02a2dc527bec5efc3f
Joe Hendrix <hendrixjoseph@aol.com>
2015-07-21 16:08:10 -0400

    Create SqlImporter.java

---
+++ src/java/edu/wright/hendrix11/familyTree/database/import/SqlImporter.java
@@ -0,0 +1,13 @@
+package edu.wright.hendrix11.familyTree.database.import;
+
+/**
+*
+* @author Joe Hendrix
+*/
+public class SqlImporter extends Importer
+{
+    public void import()
+    {
+    
+    }
+}
