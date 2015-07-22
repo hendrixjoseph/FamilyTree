66a455dec353af0b63e9a9052421a1ad1f6aeba1
Joe Hendrix <hendrixjoseph@aol.com>
2015-07-22 16:12:47 -0400

    Create Settings.java

---
+++ src/java/edu/wright/hendrix11/familyTree/entity/Settings.java
@@ -0,0 +1,13 @@
+package edu.wright.hendrix11.familyTree.entity;
+
+/**
+*
+* @author Joe Hendrix
+*/
+public class Settings
+{
+  String theme;
+  Person defaultPerson;
+  String defaultPersonType;
+  boolean viewWelcomePage;
+}
