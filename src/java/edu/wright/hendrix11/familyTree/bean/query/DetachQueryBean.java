cd18d3e4834690ee6a0538195c7899930bbb98fc
Joe Hendrix <hendrixjoseph@aol.com>
2015-07-22 12:04:07 -0400

    Create DetachQueryBean.java

---
+++ src/java/edu/wright/hendrix11/familyTree/bean/query/DetachQueryBean.java
@@ -0,0 +1,27 @@
+package edu.wright.hendrix11.familyTree.bean.query;
+
+
+/**
+ *
+ * @author Joe Hendrix <hendrix.11@wright.edu>
+ */
+@ManagedBean
+@ViewScoped
+public class DetachQueryBean extends QueryBean implements Serializable
+{   
+    @PostConstruct
+    public void initialize()
+    {
+    
+    }
+    
+    /**
+     *
+     * @param actionEvent
+     */
+    @Override
+    public void commit(ActionEvent actionEvent)
+    {   
+    
+    }
+}
