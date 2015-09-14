48f6bb9f4fea1ebf21dcbcb3a60db59d262d9cc1
Joseph Hendrix <hendrixjoseph@aol.com>
2015-09-14 13:01:30 -0400

    adding some temporary resources

---
+++ src/main/java/temp/exceptions/RollbackFailureException.java
@@ -0,0 +1,10 @@
+package temp.exceptions;
+
+public class RollbackFailureException extends Exception {
+    public RollbackFailureException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public RollbackFailureException(String message) {
+        super(message);
+    }
+}
