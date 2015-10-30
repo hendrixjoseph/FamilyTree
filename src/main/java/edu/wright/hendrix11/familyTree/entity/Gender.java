b35ce94a96a723a52f326fbfa388c4f34ed1bd3f
Joseph Hendrix <hendrixjoseph@aol.com>
2015-10-30 13:43:42 -0400

    Update Gender.java

--- src/main/java/edu/wright/hendrix11/familyTree/entity/Gender.java
+++ src/main/java/edu/wright/hendrix11/familyTree/entity/Gender.java
@@ -12,98 +12,10 @@
 
 package edu.wright.hendrix11.familyTree.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQuery;
-import javax.validation.constraints.NotNull;
-
-/**
- * @author Joe Hendrix
- */
-@Entity
-@NamedQuery(name = Gender.FIND_ALL, query = "SELECT g FROM Gender g")
-public class Gender
+public enum Gender 
 {
-    /**
-     * Specifies the {@link String} that represents the {@link javax.persistence.NamedQuery} to create a {@link
-     * javax.persistence.TypedQuery} to get all genders.
-     */
-    public static final String FIND_ALL = "Gender.findAll";
-
-    @Id
-    private char abbr;
-
-    @Column(name = "FULL_WORD")
-    @NotNull
-    private String fullWord;
-
-    /**
-     * Public no-argument constructor for JPA.
-     */
-    public Gender()
-    {
-        super();
-    }
-
-    /**
-     * @param abbr the abbreviation for the gender
-     */
-    public Gender(char abbr)
-    {
-        this.abbr = abbr;
-    }
-
-    /**
-     * Returns the abbreviation for the gender. This is typically just the first letter.
-     * 
-     * @return the abbreviation for the gender
-     * 
-     * @see #getFullWord()
-     */
-    public char getAbbr()
-    {
-        return abbr;
-    }
-
-    /**
-     * Sets the abbreviation for the gender. This is typically just the first letter.
-     * 
-     * @param abbr the abbreviation for the gender
-     */
-    public void setAbbr(char abbr)
-    {
-        this.abbr = abbr;
-    }
-
-    /**
-     * Returns the entire word for the gender. Available values include Male, Female, Other, and Unknown.
-     * 
-     * @return the entire word for the gender
-     */
-    public String getFullWord()
-    {
-        return fullWord;
-    }
-
-    /**
-     * Sets the entire word for the gender. Available values include Male, Female, Other, and Unknown.
-     * 
-     * @param fullWord the entire word for the gender
-     */
-    public void setFullWord(String fullWord)
-    {
-        this.fullWord = fullWord;
-    }
-
-    /**
-     * @return A string representation of the gender, which is just the full word.
-     *
-     * @see #getFullWord()
-     */
-    @Override
-    public String toString()
-    {
-        return fullWord;
-    }
+    Male,
+    Female,
+    Other,
+    Unknown;
 }
