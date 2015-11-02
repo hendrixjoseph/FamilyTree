cb0306a0ff3d6da8ebc80ebcc8593be15464e159
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-02 16:39:42 -0500

    Update City.java

--- src/main/java/edu/wright/hendrix11/familyTree/entity/place/City.java
+++ src/main/java/edu/wright/hendrix11/familyTree/entity/place/City.java
@@ -42,6 +42,7 @@ public class City extends Place
      * states by name.
      * 
      * For example: {@code TypedQuery<City> query = em.createNamedQuery(City.FIND_BY_NAME, City.class);}
+     *              {@code query..setParameter("name", name);
      */
     public static final String FIND_BY_NAME = "City.findByName";
 
