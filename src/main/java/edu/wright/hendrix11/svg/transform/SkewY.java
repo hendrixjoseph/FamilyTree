d33fc1f13a1a1c69f90839d76fce9bdea4d00797
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-09 14:38:01 -0500

    Create SkewY.java

---
+++ src/main/java/edu/wright/hendrix11/svg/transform/SkewY.java
@@ -0,0 +1,24 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  View the full license at:
+ *  https://github.com/hendrixjoseph/FamilyTree/blob/master/LICENSE.md
+ *
+ *  Copyright (c) 2015 Joseph Hendrix
+ *
+ *  Hosted on GitHub at https://github.com/hendrixjoseph/FamilyTree
+ *
+ */
+
+package edu.wright.hendrix11.svg.transform;
+
+/**
+ * @author Joe Hendrix
+ */
+public abstract class SkewY extends Skew
+{
+    public SkewY(Number a)
+    {
+        super(a,"Y");
+    }
+}
