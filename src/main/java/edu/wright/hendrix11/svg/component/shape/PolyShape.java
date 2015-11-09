73e24dbe03e22dc94c82d28d8eb61a128bce1876
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-09 16:05:42 -0500

    Update PolyShape.java

--- src/main/java/edu/wright/hendrix11/svg/component/shape/PolyShape.java
+++ src/main/java/edu/wright/hendrix11/svg/component/shape/PolyShape.java
@@ -21,27 +21,54 @@ import java.io.IOException;
  */
 public abstract class PolyShape extends SvgComponent
 {
-    List<Number> points = new ArrayList<>();
+    List<Point> points = new ArrayList<>();
 
     protected RoundShape(String name)
     {
         super(name);
     }
     
-    public void setPoints(List<Number> points)
+    public void setPoints(List<Point> points)
     {
         this.points = points;
+        processPoints();
     }
     
-    public void addPoint(Number point)
+    public void addPoint(Point point)
     {
         points.add(point);
+        processPoints();
     }
 
     private void processPoints()
     {
       StringBuilder sb = new StringBuilder();
       
+      for(Point point : points)
+      {
+          sb.append(point).append(" ");
+      }
       
+      sb.setLength(sb.length() - 1);
+      
+      put("points",sb.toString());
+    }
+    
+    public class Point
+    {
+        private Number x;
+        private Number y;
+        
+        public Point(Number x, Number y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+        @Override
+        public String toString()
+        {
+            return x + "," + y;
+        }
     }
 }
