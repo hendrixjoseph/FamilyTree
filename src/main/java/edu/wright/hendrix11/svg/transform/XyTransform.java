7fb24c47058b17ee5058ba8a91933e6e5941490b
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-09 14:08:20 -0500

    Create XyTransform.java

---
+++ src/main/java/edu/wright/hendrix11/svg/transform/XyTransform.java
@@ -0,0 +1,33 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  View the full license at:
+ *  https://github.com/hendrixjoseph/FamilyTree/blob/master/LICENSE.md
+ *
+ *  Copyright (c) 2015 Joseph Hendrix
+ *
+ *  Hosted on GitHub at https://github.com/hendrixjoseph/FamilyTree
+ *
+ */
+
+package edu.wright.hendrix11.svg.transform;
+
+/**
+ * @author Joe Hendrix
+ */
+public abstract class XyTransform implements Transform
+{
+    Number x;
+    Number y;
+
+    public XyTransform(Number x, Number y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public XyTransform(Number x)
+    {
+        this.x = x;
+    }
+}
