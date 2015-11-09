a7796cd530a01c2e0dda7da245afea879a7d1f12
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-09 16:07:10 -0500

    Create Polygon.java

---
+++ src/main/java/edu/wright/hendrix11/svg/component/shape/Polygon.java
@@ -0,0 +1,28 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  View the full license at:
+ *  https://github.com/hendrixjoseph/FamilyTree/blob/master/LICENSE.md
+ *
+ *  Copyright (c) 2015 Joseph Hendrix
+ *
+ *  Hosted on GitHub at https://github.com/hendrixjoseph/FamilyTree
+ *
+ */
+
+package edu.wright.hendrix11.svg.component.shape;
+
+import javax.faces.context.ResponseWriter;
+
+import java.io.IOException;
+
+/**
+ * @author Joe Hendrix
+ */
+public class Polygon extends PolyShape
+{
+    protected Polygon()
+    {
+        super("polygon");
+    }
+}
