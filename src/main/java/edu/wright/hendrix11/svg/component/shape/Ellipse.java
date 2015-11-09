c78857f6a3bc08f3cc15f1478d37d122b55153bc
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-09 15:22:37 -0500

    Create Ellipse.java

---
+++ src/main/java/edu/wright/hendrix11/svg/component/shape/Ellipse.java
@@ -0,0 +1,38 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  View the full license at:
+ *  https://github.com/hendrixjoseph/FamilyTree/blob/master/LICENSE.md
+ *
+ *  Copyright (c) 2015 Joseph Hendrix
+ *
+ *  Hosted on GitHub at https://github.com/hendrixjoseph/FamilyTree
+ *
+ */
+
+package edu.wright.hendrix11.svg.component.shape;
+
+import javax.faces.context.ResponseWriter;
+
+import java.io.IOException;
+
+/**
+ * @author Joe Hendrix
+ */
+public class Ellipse extends RoundShape
+{
+    public Ellipse()
+    {
+      super("ellipse");
+    }
+
+    public void setXRadius(Number rx)
+    {
+        put("rx",rx);
+    }
+    
+    public void setYRadius(Number ry)
+    {
+        put("ry",ry);
+    }
+}
