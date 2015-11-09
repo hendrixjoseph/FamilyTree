95ca092fbbbbc5ed8ed193005b13d5d4cd44286d
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-09 14:37:19 -0500

    Create SkewX.java

---
+++ src/main/java/edu/wright/hendrix11/svg/transform/SkewX.java
@@ -0,0 +1,24 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  View the full license at:
+ *  https://github.com/hendrixjoseph/FamilyTree/blob/master/LICENSE.md
+ *
+ *  Copyright (c) 2015 Joseph Hendrix
+ *
+ *  Hosted on GitHub at https://github.com/hendrixjoseph/FamilyTree
+ *
+ */
+
+package edu.wright.hendrix11.svg.transform;
+
+/**
+ * @author Joe Hendrix
+ */
+public abstract class SkewX extends Skew
+{
+    public SkewX(Number a)
+    {
+        super(a,"X");
+    }
+}
