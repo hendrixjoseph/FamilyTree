96cf9a20bb65e4afb2c9f31d0705ed0662f95d0c
Joseph Hendrix <hendrixjoseph@aol.com>
2015-11-23 11:51:57 -0500

    Create XAxis.java

---
+++ src/main/java/edu/wright/hendrix11/c3/Axis/XAxis.java
@@ -0,0 +1,30 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  View the full license at:
+ *  https://github.com/hendrixjoseph/FamilyTree/blob/master/LICENSE.md
+ *
+ *  Copyright (c) 2015 Joseph Hendrix
+ *
+ *  Hosted on GitHub at https://github.com/hendrixjoseph/FamilyTree
+ *
+ */
+
+package edu.wright.hendrix11.c3.Axis;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+/**
+ * @author Joe Hendrix
+ */
+public class XAxis extends Axis
+{
+    private boolean localTime;
+
+    public XAxis()
+    {
+        super("x");
+    }
+}
