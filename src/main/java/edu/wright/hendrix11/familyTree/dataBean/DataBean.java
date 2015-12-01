ac27381de20c33474db6a2191d587c2e9b596552
Joseph Hendrix <hendrixjoseph@aol.com>
2015-12-01 16:15:29 -0500

    Update DataBean.java

--- src/main/java/edu/wright/hendrix11/familyTree/dataBean/DataBean.java
+++ src/main/java/edu/wright/hendrix11/familyTree/dataBean/DataBean.java
@@ -94,6 +94,11 @@ public class DataBean<E, K>
 
     /**
      * Returns the current sort String.
+     * <p>
+     * The sort string is actually a query of sorts (pun intended?) where, when {@link #list} 
+     * is called, is sorted in a way where the string is seperated into a string array by the period (".")
+     * character, and then the query result is sorted by the first element of the string array, then the
+     * next element, and so on.
      * 
      * @return the current sort String
      */
@@ -104,6 +109,11 @@ public class DataBean<E, K>
 
     /**
      * Sets the sort String.
+     * <p>
+     * The sort string is actually a query of sorts (pun intended?) where, when {@link #list} 
+     * is called, is sorted in a way where the string is seperated into a string array by the period (".")
+     * character, and then the query result is sorted by the first element of the string array, then the
+     * next element, and so on.
      * 
      * @param sort the sort String
      */
