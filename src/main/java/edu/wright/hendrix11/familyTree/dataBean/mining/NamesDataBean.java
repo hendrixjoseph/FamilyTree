1a9b9b03b3d23d64069a5eec9154c0c57b6a4795
Joseph Hendrix <hendrixjoseph@aol.com>
2015-12-01 14:27:56 -0500

    Update NamesDataBean.java

--- src/main/java/edu/wright/hendrix11/familyTree/dataBean/mining/NamesDataBean.java
+++ src/main/java/edu/wright/hendrix11/familyTree/dataBean/mining/NamesDataBean.java
@@ -16,7 +16,7 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,11 +29,45 @@ public class NamesDataBean
     @PersistenceContext(unitName = "edu.wright.hendrix11.familyTree")
     private EntityManager em;
 
+    /**
+     * Returns a map of first names to the number of occurrences of that first name.
+     * <p>
+     * Internally this is a {@link HashMap}. It only returns at most the top 20 names.
+     * Since the entire name is stored in the database as a single field, a regular expression
+     * is used in a view inside the database itself to count the first names.
+     * <p>
+     * The select statement defining the view appears as follows:
+     * <blockquote><pre>{@code
+     * SELECT COUNT(FIRST_NAME) COUNT, FIRST_NAME
+     * FROM (SELECT REGEXP_SUBSTR(NAME,'^\w+') AS FIRST_NAME FROM PERSON)
+     * GROUP BY FIRST_NAME
+     * ORDER BY COUNT DESC;
+     * }</pre></blockquote>
+     * 
+     * @return a map of first names to the number of occurrences of that first name
+     */ 
     public Map<String, Integer> firstNameFrequency()
     {
         return nameFrequency("FIRST");
     }
-
+    
+    /**
+     * Returns a map of last names to the number of occurrences of that last name.
+     * <p>
+     * Internally this is a {@link HashMap}. It only returns at most the top 20 names.
+     * Since the entire name is stored in the database as a single field, a regular expression
+     * is used in a view inside the database itself to count the last names.
+     * <p>
+     * The select statement defining the view appears as follows:
+     * <blockquote><pre>{@code
+     * SELECT COUNT(LAST_NAME) COUNT, LAST_NAME
+     * FROM (SELECT REGEXP_SUBSTR(NAME,'\w+$') AS LAST_NAME FROM PERSON)
+     * GROUP BY LAST_NAME
+     * ORDER BY COUNT DESC;
+     * }</pre></blockquote>
+     * 
+     * @return a map of last names to the number of occurrences of that last name
+     */ 
     public Map<String, Integer> lastNameFrequency()
     {
         return nameFrequency("LAST");
@@ -41,7 +75,7 @@ public class NamesDataBean
 
     private Map<String, Integer> nameFrequency(String lastOrFirst)
     {
-        Map<String, Integer> map = new LinkedHashMap<>();
+        Map<String, Integer> map = new HashMap<>();
 
         String query = "SELECT * FROM " + lastOrFirst + "_NAME_COUNT_VIEW";
 
