18071c7df7617f2593615851fe60974e4973fb9e
Joseph Hendrix <hendrixjoseph@aol.com>
2015-12-03 12:35:28 -0500

    Create YPadding.java

---
+++ src/main/java/edu/wright/hendrix11/d3/chart/axis/YPadding.java
@@ -0,0 +1,39 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  View the full license at:
+ *  https://github.com/hendrixjoseph/FamilyTree/blob/master/LICENSE.md
+ *
+ *  Copyright (c) 2015 Joseph Hendrix
+ *
+ *  Hosted on GitHub at https://github.com/hendrixjoseph/FamilyTree
+ *
+ */
+
+package edu.wright.hendrix11.d3.chart.axis;
+
+/**
+ * The padding for the y-axis will create more space on the edge of the axis. 
+ * Top and bottom will be treated as pixels.
+ */
+public class YPadding
+{
+    int top;
+    int bottom;
+
+    /**
+     * @param top the padding on the top, in pixels
+     * @param  the padding on the bottom, in pixels
+     */
+    public YPadding(int top, int bottom)
+    {
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "{top:" + top + ",bottom:" + bottom + "}";
+    }
+}
