b5ef199a64e2f7d9523bd0e07c3fe9cd5142b55e
Joseph Hendrix <hendrixjoseph@aol.com>
2015-12-03 12:37:47 -0500

    Create Padding.java

---
+++ src/main/java/edu/wright/hendrix11/d3/chart/axis/Padding.java
@@ -0,0 +1,18 @@
+/*
+ *  The MIT License (MIT)
+ *
+ *  View the full license at:
+ *  https://github.com/hendrixjoseph/FamilyTree/blob/master/LICENSE.md
+ *
+ *  Copyright (c) 2015 Joseph Hendrix
+ *
+ *  Hosted on GitHub at https://github.com/hendrixjoseph/FamilyTree
+ *
+ */
+
+package edu.wright.hendrix11.d3.chart.axis;
+
+public interface Padding
+{
+
+}
